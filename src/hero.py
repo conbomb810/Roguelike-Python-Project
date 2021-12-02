fed068eab1cd86cc09e18110083439567044f6b5
Conner <csmit126@binghamton.edu>
2021-12-01 19:39:53 -0500

    added monster and hero health bar and attack

--- src/hero.py
+++ src/hero.py
@@ -2,52 +2,59 @@ import pygame
 import random
 
 class Hero(pygame.sprite.Sprites):
-   def __init__(self):
+   def __init__(self, name, health, potion, strength):
       super().__init__()
-      self.direction = 'R'
       self.image = pygame.image.load('assets/samarai.png').convert_alpha()
       self.rect = self.image.get_rect()
       self.rect.inflate_ip(-25, 25)
-      self.speed = 15
-      self.rect.x = 10
-      self.rect.y = 10
-      self.hitbox = (40, 20, 20, 50) 
-      self.health = 200
-      self.max_health
+      self.rect.x = 400
+      self.rect.y = 300
+      self.alive = True
+      self.name = name
+      self.strength = strength
+      self.potion = potion 
+      self.health = health
+      self.max_health = self.health
       self.bar_len = 400
       self.health.ratio = self.max_health/self.bar_len
+   #all stats can change based on what class is chosen, this is for testing. 
    """sets image for characted adjust size, also sets size of hitbox for collision attacks"""
    
-   def move(self.direction)
-      if direction == "U":
-         self.rect.y -= self.speed
-      elif direction == "D":
-         self.rect.y += self.speed
-      elif direction == "L":
-         self.rect.x += self.speed
-      elif+direction == "R":
-         self.rect x -= self.speed
-   """the vertical and horizontal movement for character"""
-  
-   def getDamage(self, amount):
+   def draw(self)
+      self.blit(self.image, (self.rect.x, self.rect.y))
+
+   
+   def update(self):
+      def self.hero_health()
+
+   def attack(self, monster) #damage to monster
+      rand = random.randint(-50, 50)
+      damage = self.strength + rand
+      monster.health -= damage
+      if monster.health < 1:
+         monster.health = 0
+    
+         
+
+   def get_damage(self, amount):
       if self.health > 0:
          self.current_health -+ amount
       if self.health =< 0:
          self.health = 0
 
 """sets damage"""
-   def getHealth(self, amount)
+   def get_health(self, amount)
       if self.health < self.max_health:
          self.health += amount
       if self.health >= self.max_health:
          self.health = self.max_health
 #for getdamage and get healthfunc, must be added to event loop. when monster attacks hero health bar must go down
 """keeps health inside bar"""
-   def heroHealth(self)
+   def hero_health(self)
       pygmae.draw.rect(screen, (250, 0, 0), (10, 10, self.health/self.health_ratio, 25)
       pygame.draw.rect(screen, (250, 250, 250), (10, 10, self.bar_len, 25), 4)
-         
-   def healthBar(self)
+      #values must be edited for size of window
+
    def attack(self)
       
    def defend(self)
--- src/monster.py
+++ src/monster.py
@@ -2,16 +2,53 @@ import pygame
 import random
 class monster(pygame.sprite.Sprite):
    
-   def __init__(self, health, start_coordx, start_coordy):
+   def __init__(self, health, x, y):
       super().__init__()
       self.health = 200
       self.image = pygame.image.load('assets/monster.png').convert_alpha()
       self.rect = self.image.get_rect()
       self.rect.inflate_ip(25, 25)
-      self.speed = 10
-      self.rect.x = start_coordx
-      self.rect.y = start_coordy 
+      self.alive = True
+      self.rect.x = x
+      self.rect.y = y 
+      self.health = health
+      self.max_health = self.health
+      self.bar_len = 400
+      self.health.ratio = self.max_health/self.bar_len
       """setting parameters for starting position and also setting the health of the monster."""
+   
+   def draw(self)
+      self.blit(self.image, (self.rect.x, self.rect.y))
+
+   def attack(self, hero) #damage
+      rand = random.randint(-50, 50)
+      damage = self.strength + rand
+      hero.health -= damage
+      if hero.health < 1:
+         hero.health = 0
+
+   def update(self):
+      def self.hero_health()
+
+   def get_damage(self, amount):
+      if self.health > 0:
+         self.current_health -+ amount
+      if self.health =< 0:
+         self.health = 0
+
+"""sets damage"""
+   def get_health(self, amount)
+      if self.health < self.max_health:
+         self.health += amount
+      if self.health >= self.max_health:
+         self.health = self.max_health
+#for getdamage and get healthfunc, must be added to event loop. when monster attacks hero health bar must go down
+"""keeps health inside bar"""
+   def hero_health(self)
+      pygmae.draw.rect(screen, (250, 0, 0), (10, 10, self.health/self.health_ratio, 25)
+      pygame.draw.rect(screen, (250, 250, 250), (10, 10, self.bar_len, 25), 4)
+      #values must be edited for size of window   
+
    def attack(self)
    """will be collision attack"""   
    def move(self)
