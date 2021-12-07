3f767f19932a97a60456896dd112a7da604f29f7
Adejo Ibrahim <aibrah10@binghamton.edu>
2021-12-07 15:08:29 -0500

    fixed syntax errors

--- src/score.py
+++ src/score.py
@@ -1,20 +1,19 @@
 #this tracks the score that the player acquires during the game
-from src import hero.py
-from src import monster.py
+from src import hero
+from src import monster
 
 #this class needs an image and rect variable, find an image to use for it, probably a font object
 #this should also export the score to the highscore json file ONLY IF score > highscore
 class score:
 	def __init__(self):
 		self.start_score = 0
-      self.increment = 100
-      self.bonus = hero.health // 2
-      
-
-   def update(self):
-      if monster.health < 1:
-         self.score += self.increment
-         self.score += self.bonus
-      return self.score
+		self.increment = 100
+		self.bonus = hero.health // 2
+	
+	def update(self):
+		if monster.health < 1:
+			self.score += self.increment
+			self.score += self.bonus
+		return self.score
          
       
